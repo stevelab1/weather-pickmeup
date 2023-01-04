4655e6cc13347a1049cd4e5b85d468e00fd15afe
LR.btcmp <loretta.rehm@gmail.com>
2023-01-04 17:52:35 -0300

    Fixed the giphy search logic

--- moodclass.js
+++ moodclass.js
@@ -1,39 +1,36 @@
 var API_KEY = "l6uqF7sCQPq99XoWHRKpST6mj2kZr5jK";
 
-// Get GIF related to location
-var queryLocation = "London";
-var queryMood = "happy";
+function getGIF() { // Get GIF related to location
+    var queryLocation = "London";
+    var queryMood = "happy";
 
-var searchQuery = trim(queryLocation) + trim(queryMood);
+    var searchQuery = queryLocation.trim() // + queryMood.trim();
 
-var goodWeather = [];
-var badWeather = [];
+    var goodWeather = [];
+    var badWeather = [];
 
-//l6uqF7sCQPq99XoWHRKpST6mj2kZr5jK&q=London
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + API_KEY + "&q=" + searchQuery + "&limit=1&offset=0&rating=g&lang=en";
+    // l6uqF7sCQPq99XoWHRKpST6mj2kZr5jK&q=London
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + API_KEY + "&q=" + searchQuery + "&limit=1&offset=0&rating=g&lang=en";
 
-console.log(queryURL);
-
-fetch(queryURL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      var gifURL = data.url;
-      var currentHumidity = data.main.humidity;
-      var currentWindSpeed = data.wind.speed;
-      var currentWeatherIcon = data.weather[0].icon;
-
-      // Display current weather and icons on index.html
-      function displayCurrentWeather() {
-        $("#city-name-display").text(city);
-        $("#temperature").text(currentTemp);
-        $("#humidity").text(currentHumidity);
-        $("#wind-speed").text(currentWindSpeed);
-        var iconTemplate = ``;
-        document.getElementById("weather-icon").innerHTML = "";
-        iconTemplate = `<img src="https://openweathermap.org/img/wn/${currentWeatherIcon}.png" alt="weather-icon" id="current-weather-icon">`;
-        document.getElementById("weather-icon").innerHTML += iconTemplate;
-      }
-      displayCurrentWeather();
+    console.log(queryURL);
+    // Creating an AJAX call for the specific movie button being clicked
+    $.ajax({url: queryURL, method: "GET"}).then(function (response) {
+        // console.log(JSON.stringify(response));
+        // Check if a gif was returned at all
+        if (response.data.length > 0) { // Retrieving the URL for the gif
+            var gifURL = response.data[0].images.downsized.url;
+            appendGIF(gifURL);
+        } else { // If no gif was found for the specified criteria, then just show a default gif
+            var gifURL = "./assets/img/cat-hug.gif";
+            appendGIF(gifURL);
+        }
     });
+}
+
+function appendGIF(gifURL) { // Creating an element to hold the gif
+    var image = $("<img src=" + gifURL + ">");
+    // Appending the image
+    $("#suggested-gif").append(image);
+}
+
+getGIF();
